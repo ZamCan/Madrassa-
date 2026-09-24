32b1485fd397a9209e4db0d91f0b4590e3c9ac07
ZamCan <zamcannexus@gmail.com>
2026-09-24 11:52:41 +0300

    Fix Solo Qibla header image import

--- app/src/main/java/com/zamcan/madrassa/SoloLearningActivity.java
+++ app/src/main/java/com/zamcan/madrassa/SoloLearningActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
